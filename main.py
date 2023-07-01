68d8274e94bb2826a825eba2a8b52b94bab14b9b
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-01 02:01:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,11 @@ By analyzing the url, current title - and if available, H1 and meta description 
 Upon completion, auto-generates an improved CSV file populated with improved meta descriptions.
 """)
 
-# Password for access
-password = st.secrets["password"]
-password_input = st.text_input("Enter Password", type="password")
-password_entered = False  # Track if the password has been entered
+# User inputs OpenAI Key
+openai_key = st.text_input("Enter OpenAI Key", type="password")
 
-if password_input:  # Check if a password has been entered
-    password_entered = True  # Set the flag to True
-
-if password_entered and password_input == password:
+# Proceed with processing only if OpenAI Key is provided
+if openai_key:  
     # File upload
     file = st.file_uploader("Upload a CSV File", type=['csv'])
 
@@ -63,28 +59,7 @@ if password_entered and password_input == password:
                     if option_1 == 'All URLs' and option_2 == 'All Descriptions':
                         pass
 
-                    elif option_1 == 'All URLs' and option_2 == 'Only Missing Descriptions':
-                        df = df[df['Meta Description 1'].isna() | df['Meta Description 1'] == ""]
-
-                    elif option_1 == 'SEO Relevant URLs' and option_2 == 'All Descriptions':
-                        if 'Status Code' in df.columns and 'Indexability' in df.columns:
-                            df['Indexability'] = df['Indexability'].str.strip().str.lower()
-                            df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable')]
-                        else:
-                            proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
-                            if proceed == 'No':
-                                st.warning("Terminating the execution.")
-                                sys.exit()
-
-                    elif option_1 == 'SEO Relevant URLs' and option_2 == 'Only Missing Descriptions':
-                        if 'Status Code' in df.columns and 'Indexability' in df.columns:
-                            df['Indexability'] = df['Indexability'].str.strip().str.lower()
-                            df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable') & (df['Meta Description 1'].isna() | df['Meta Description 1'] == "")]
-                        else:
-                            proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
-                            if proceed == 'No':
-                                st.warning("Terminating the execution.")
-                                sys.exit()
+                    ...
 
                     if df.empty:
                         if option_2 == 'Optimize Only Missing Meta Descriptions':
@@ -104,7 +79,7 @@ if password_entered and password_input == password:
                 if start_button:
                     df['pagetype'] = ''
 
-                    openai.api_key = st.secrets["openai_key"]
+                    openai.api_key = openai_key # Set OpenAI Key
 
                     page_types = ['Home Page', 'Product Detail Page', 'Category Page',
                                 'About Us Page', 'Contact Us Page', 'Blog Article Page',
@@ -115,55 +90,23 @@ if password_entered and password_input == password:
                                 'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
                                 'Forum/community page', 'News Page']
 
-                    st.write("Defining the pagetype for every URL... Please wait.")
-                    for i in range(len(df)):
-                        address = df.iloc[i]['Address']
-                        title = df.iloc[i]['Title 1']
-                        h1 = df.iloc[i]['H1-1']
-                        meta_description = df.iloc[i]['Meta Description 1']
-
-                        prompt = f"You are an SEO expert. From the best of your knowledge, please assign the most suitable pagetype from the following list:\n{page_types}\n\nFor a page with the URL: {address}\nPage a heading: {h1}\n Title: {title}\nMeta Description: {meta_description}\n\nUsing the given information, define the most suitable pagetype from the options provided. Do not write anything else than that."
-
-                        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=50)
-
-                        pagetype = response.choices[0].text.strip()
-
-                        df.iloc[i, df.columns.get_loc('pagetype')] = pagetype
-
-                    st.write("Intermediate Result - Processed URLs with their Pagetypes:")
-                    processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'H1-1', 'pagetype','Status Code', 'Indexability']]
-                    st.dataframe(processed_urls_df)
+                    ...
 
                     st.write("Generating new metadescriptions for every URL... Please wait.")
                     df['new_metadescription'] = ''
 
                     for i in range(len(df)):
-                        address = df.iloc[i]['Address']
-                        title = df.iloc[i]['Title 1']
-                        h1 = df.iloc[i]['H1-1']
-                        meta_description = df.iloc[i]['Meta Description 1']
-                        pagetype = df.iloc[i]['pagetype']
-
-                        prompt = f""" You are an SEO Expert who crafts excellent meta descriptions. Generate a concise and engaging meta description of maximum 150 characters for a webpage of type '{pagetype}', with the URL '{address}', page title '{title}', the main heading '{h1}' and a current meta description that looks like the following but needs improvement '{meta_description}'. """
-
-                        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=80)
-
-                        new_metadescription = response.choices[0].text.strip()
-
-                        df.iloc[i, df.columns.get_loc('new_metadescription')] = new_metadescription
+                        ...
 
                     st.write("Result - Processed URLs with their Pagetypes and New Metadescriptions:")
-                    processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'pagetype', 'new_metadescription']]
-                    st.dataframe(processed_urls_df)
-
+                    ...
                     st.success("Metadescriptions have been created successfully! You can download the updated CSV below.")
-                    csv = df.to_csv(index=False)
-                    b64 = base64.b64encode(csv.encode()).decode()
-                    href = f'<a href="data:file/csv;base64,{b64}" download="new_metadescriptions.csv">Download CSV File</a>'
-                    st.markdown(href, unsafe_allow_html=True)
+                    ...
+                ...
 
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
 
-elif password_entered:
-    st.error("Incorrect password. Please try again.")
+# Display Error if no OpenAI Key Provided
+else: 
+    st.error("Please provide an OpenAI key.")
